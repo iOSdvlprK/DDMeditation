bb42c5239ebb28846e902b9f47240633090ad2b7
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-09-03 21:50:34 +0900

    Initial Commit

---
+++ DDMeditation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DDMeditation
+//
+//  Created by joe on 9/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DDMeditation/DDMeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDMeditationApp.swift
+//  DDMeditation
+//
+//  Created by joe on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DDMeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
